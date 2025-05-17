b497aca9dfe9160417888adb62063c4d89b08359
nithig <154483033+nithig@users.noreply.github.com>
2025-05-17 09:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name: string, age: number)
+	get name(): string
+	get age(age: number)
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(): void
+	<name> is studying
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(): void
+	<name> is teaching
+}
 
 // Do not change the code below this line
 window.Person = Person;
